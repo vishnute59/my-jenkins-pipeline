1ba4a38e300fa782928a3c8668f8808a534b62a0
vishnute59 <102622085+vishnute59@users.noreply.github.com>
2024-10-06 04:41:47 +0530

    Delete 1-terraform-jenkins-server directory

--- 1-terraform-jenkins-server/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ec2_public_ip" {
-  value = aws_instance.my-server.public_ip
-}
--- 1-terraform-jenkins-server/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "availability_zone" {}
-variable "env_prefix" {}
-variable "instance_type" {}
